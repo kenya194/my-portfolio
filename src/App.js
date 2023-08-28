bff4b1c31f90708a760fd88733a47c3fbc9736cf
kenya194 <ernestbv553@gmail.com>
2023-08-28 14:19:09 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react'
-import Header from './components/header/Header'
+
+import Header from './components/header/Header';
+import Contact from './components/contact/Contact';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Portfolio from './components/portfolio/Portfolio';
 
 // import './App.css';
 
@@ -7,6 +14,12 @@ function App() {
   return (
     <>
       <Header />
+      <Contact />
+      <About />
+      <Experience />
+      <Footer />
+      <Navbar />
+      <Portfolio />
     </>
   );
 }
