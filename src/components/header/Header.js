353886a0ac65db521b074da202e8ead4188e6e7b
kenya194 <ernestbv553@gmail.com>
2023-08-30 21:53:08 +0000

    commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA.js'
+import profile from '../../Assets/Mine.jpg'
+import HeaderSocials from './HeaderSocials'
 
 
 
 const Header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Amankwah Ernest</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={profile} alt='Profile'/>
+        </div>
+
+
+      </div>
+    </header>
   )
 }
 
