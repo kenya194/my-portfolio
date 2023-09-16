2061bf90342d882cea453adda560d3e03add731b
kenya194 <ernestbv553@gmail.com>
2023-09-16 19:59:13 +0000

    commit

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import './Experience.css'
+import{BsFillPatchCheckFill} from 'react-icons/bs'
 
 
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>Skills acquired</h5>
+      <h2> My skills</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>HTML</h4>
+                <small className='text-light'> Experienced</small>
+            </article>
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>CSS</h4>
+                <small className='text-light'> Intermediate</small>
+            </article>
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4> JavaScript</h4>
+                <small className='text-light'> Experienced</small>
+            </article>
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>Bootstrap</h4>
+                <small className='text-light'> Intermediate</small>
+            </article>
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>React Js</h4>
+                <small className='text-light'> Intermediate</small>
+            </article>
+            
+          </div>
+        </div>
+
+      </div>
     </section>
   )
 }
