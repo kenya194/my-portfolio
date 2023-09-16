e7c0d3272c2de514f632d21cd7e86ca9013c3468
kenya194 <ernestbv553@gmail.com>
2023-09-16 21:31:34 +0000

    commit

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -42,6 +42,38 @@ const Experience = () => {
           </div>
         </div>
 
+            <div className="experience__otherSkills">
+              <h3>Other Skills</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>Communication</h4>
+                <small className='text-light'> Proficient</small>
+            </article>
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>Leadership</h4>
+                <small className='text-light'> Experienced</small>
+            </article>
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4> Writing</h4>
+                <small className='text-light'> Good</small>
+            </article>
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>Spanish</h4>
+                <small className='text-light'> Level - B2</small>
+            </article>
+            <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>German</h4>
+                <small className='text-light'> Level - B2</small>
+            </article>
+
+            </div>
+        </div>
+
       </div>
     </section>
   )
