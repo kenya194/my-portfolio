398cfaa3755f63cae5a534a42cc8b0e70000662c
kenya194 <ernestbv553@gmail.com>
2023-09-17 23:14:39 +0000

    commit

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -1,10 +1,86 @@
 import React from 'react'
 import './Service.css'
+import{BsFillClipboard2CheckFill} from 'react-icons/bs'
+import{BsFillFileCodeFill} from 'react-icons/bs'
+import{BsVectorPen} from 'react-icons/bs'
 
 const Service = () => {
   return (
     <section id='services'>
-        Services
+        <h5>What i can offer</h5>
+        <h2>Services</h2>
+
+        <div className="container services__container">
+          <div className="services__sales">
+            <h3>Sales Marketing</h3>
+              <div className="services__content">
+                <article className="services__details">
+                  <BsFillClipboard2CheckFill />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsFillClipboard2CheckFill />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsFillClipboard2CheckFill />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsFillClipboard2CheckFill />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                
+              </div>
+          </div>
+
+          <div className="services__web">
+            <h3>Web Development</h3>
+              <div className="services__content">
+                <article className="services__details">
+                  <BsFillFileCodeFill />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsFillFileCodeFill />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsFillFileCodeFill />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsFillFileCodeFill />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                
+              </div>
+          </div>
+
+          <div className="services__writing">
+            <h3>Writing</h3>
+              <div className="services__content">
+                <article className="services__details">
+                  <BsVectorPen />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsVectorPen />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsVectorPen />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                <article className="services__details">
+                  <BsVectorPen />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto?</p>
+                </article>
+                
+              </div>
+          </div>
+
+        </div>
     </section>
   )
 }
