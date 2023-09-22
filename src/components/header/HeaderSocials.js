0f6a8d6d10e5a74a92c398a42afe0c6217353fc2
kenya194 <ernestbv553@gmail.com>
2023-09-22 15:32:44 +0000

    commit

--- src/components/header/HeaderSocials.js
+++ src/components/header/HeaderSocials.js
@@ -6,9 +6,9 @@ import {BsInstagram} from "react-icons/bs"
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com' target='__blank'> <BsLinkedin/> </a>
-        <a href='https://github.com' target='__blank'> <BsGithub/> </a>
-        <a href='https://instagram.com' target='__blank'> <BsInstagram/> </a>
+        <a href='https://www.linkedin.com/in/ernest-amankwah-41746320b' target='__blank'> <BsLinkedin/> </a>
+        <a href='https://github.com/kenya194' target='__blank'> <BsGithub/> </a>
+        <a href='https://instagram.com/black_mirth?igshid=OGQ5ZDc2ODk2ZA==' target='__blank'> <BsInstagram/> </a>
     </div>
   )
 }
