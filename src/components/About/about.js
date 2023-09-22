ecfc0b1f7e70b56e4e6df4eb89aa28a8aa4cee2c
kenya194 <ernestbv553@gmail.com>
2023-09-22 23:29:24 +0000

    commit

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,10 +1,54 @@
 import React from 'react'
 import './About.css'
+import pic from '../../Assets/pexels-oussama-bergaoui-463684.jpg'
+import {FaAward} from 'react-icons/fa'
+import {ImBooks} from 'react-icons/im'
+import {FaFreeCodeCamp} from 'react-icons/fa'
+
 
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h5>Get to know </h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+      <div className='about__me'>
+        <div className="about__me-image">
+            <img src={pic} alt='pic' />
+        </div>
+      </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5> Experience</h5>
+              <small>1+ years of coding </small>
+            </article>
+
+            <article className='about__card'>
+              <ImBooks className='about__icon'/>
+              <h5> Books</h5>
+              <small> The mountain is you</small>
+            </article>
+
+            <article className='about__card'>
+              <FaFreeCodeCamp className='about__icon'/>
+              <h5> Projects</h5>
+              <small>15+ projects </small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati blanditiis eum deserunt magni quo aspernatur dicta adipisci culpa, cupiditate reprehenderit? Repellendus incidunt deserunt debitis totam expedita tempora eius? Rem, error!
+          </p>
+
+          <a href='#contact'className='btn btn-primary'>Let's talk</a>
+
+        </div>
+      </div>
+      
     </section>
   )
 }
--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import './Contact.css'
+import {MdOutlineMailOutline} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import {FaSnapchat} from 'react-icons/fa'
+
 
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMailOutline/>
+            <h4>Email</h4>
+            <h5>eamankwah698@gmail.com</h5>
+            <a href='mailto:eamankwah698@gmail.com' target='__blank'>send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp/>
+            <h4>Whatsapp</h4>
+            <a href='https://api.whatsapp.com/send?phone=+233554184099'target='__blank'>send a text</a>
+          </article>
+
+          <article className="contact__option">
+            <FaSnapchat/>
+            <h4>Snapchat</h4>
+            <h5>Join the fun on snapchat &#x1F4F7;  </h5>
+            <a href='https://t.snapchat.com/3vGiEXeQ' target='__blank'>Add up </a>
+          </article>
+        </div>
+      </div>
     </section>
   )
 }
