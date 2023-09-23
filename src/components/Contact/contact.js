36a5e2134ecaa55807300bc504dc322c39faa4d3
kenya194 <ernestbv553@gmail.com>
2023-09-23 17:48:42 +0000

    commit

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -14,25 +14,32 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineMailOutline/>
+            <MdOutlineMailOutline className='contact__option-icons'/>
             <h4>Email</h4>
             <h5>eamankwah698@gmail.com</h5>
             <a href='mailto:eamankwah698@gmail.com' target='__blank'>send a message</a>
           </article>
 
           <article className="contact__option">
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icons'/>
             <h4>Whatsapp</h4>
             <a href='https://api.whatsapp.com/send?phone=+233554184099'target='__blank'>send a text</a>
           </article>
 
           <article className="contact__option">
-            <FaSnapchat/>
+            <FaSnapchat className='contact__option-icons'/>
             <h4>Snapchat</h4>
             <h5>Join the fun on snapchat &#x1F4F7;  </h5>
             <a href='https://t.snapchat.com/3vGiEXeQ' target='__blank'>Add up </a>
           </article>
         </div>
+
+        <form action=''>
+          <input type="text" name='name' placeholder='Your full name'  className='placeHolder-text' required />
+          <input type="email" name='email' placeholder='Your Email' className='placeHolder-text' required />
+          <textarea name='message' rows='7' placeholder='Your Message' className='placeHolder-text' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
