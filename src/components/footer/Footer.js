98a506ab9f7185600894f28e8e0eedf823d28e55
kenya194 <eamankwah698@gmail.com>
2023-10-06 17:40:41 -0700

    commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import './Footer.css'
+import {BsTwitter} from 'react-icons/bs'
 
 const Footer = () => {
   return (
     <footer>
-      Foot
-      </footer>
+      <a href='#header' className='footer__logo'>Black Mirth</a>
+
+      <ul className='permalinks'>
+        <li><a href='#header'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#service'>Service</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+        <div className='footer__socials'>
+          <a href='https://twitter.com'><BsTwitter/></a>
+        </div>
+
+        <div className='footer__copyright'>
+          <small> Ernest's personal site. All rights reserved.</small>
+        </div>
+      
+    </footer>
   )
 }
 
