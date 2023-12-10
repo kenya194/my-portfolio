663633fc5bbcb22dcd317b0a89be1b6e3703bbff
kenya194 <eamankwah698@gmail.com>
2023-12-10 01:03:26 +0000

    image

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './cta.js'
-import profile from '../../assets/littleboy.jpg'
+import profile from '../../assets/blackboy.jpg'
 import HeaderSocials from './headerSocials'
 
 
